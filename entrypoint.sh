a2c92c13f210bc5aab37e1938a955abda5756d65
alnbastos <alinebastos23@outlook.com>
2026-02-07 16:05:06 -0300

    chore: correção da configuração do aerich

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,21 @@
 set -e
 
 echo "⏳ Waiting for database..."
-sleep 2
+until pg_isready -h db -p 5432 -U postgres; do
+  echo "Postgres is unavailable - sleeping"
+  sleep 2
+done
+
+# inicializa Aerich apenas se ainda não existir config
+if [ ! -f "./migrations/aerich.ini" ]; then
+    echo "🚀 Initializing Aerich..."
+    aerich init -t app.configs.database.TORTOISE_ORM
+    aerich init-db
+fi
 
 echo "🚀 Running migrations..."
-aerich upgrade || aerich init -t app.configs.database.TORTOISE_ORM
+aerich migrate
+aerich upgrade
 
 echo "✅ Starting API..."
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
